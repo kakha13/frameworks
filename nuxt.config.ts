93be04948d0eb6f69ef87ae8cae2ce4689f4cdda
KAKHA13 <kakhagiorgashvili@gmail.com>
2022-12-27 02:11:09 +0400

    udpate config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  // css: ["~/node_modules/bootstrap/dist/css/bootstrap.min.css"],
+  css: ["~/node_modules/bootstrap/dist/css/bootstrap.min.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
